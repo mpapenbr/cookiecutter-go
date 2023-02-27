cd251eb731069e66c5505895be79ee7a68e8d112
mpapenbr <Markus.Papenbrock+github@gmail.com>
2023-02-27 22:12:33 +0100

    chg: added more default actions in rootCmd

--- {{ cookiecutter.__project_name }}/cmd/root.go
+++ {{ cookiecutter.__project_name }}/cmd/root.go
@@ -52,6 +52,9 @@ func init() {
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+	// add commands here
+	// e.g. rootCmd.AddCommand(sampleCmd.NewSampleCmd())
 }
 
 // initConfig reads in config file and ENV variables if set.
@@ -66,16 +69,21 @@ func initConfig() {
 
 		// Search config in home directory with name ".{{ cookiecutter.__project_name }}" (without extension).
 		viper.AddConfigPath(home)
+		viper.AddConfigPath(".")
 		viper.SetConfigType("yaml")
 		viper.SetConfigName(".{{ cookiecutter.__project_name }}")
 	}
 
+	viper.SetEnvPrefix(envPrefix)
 	viper.AutomaticEnv() // read in environment variables that match
 
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
 		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
 		bindFlags(rootCmd, viper.GetViper())
+		for _, cmd := range rootCmd.Commands() {
+			bindFlags(cmd, viper.GetViper())
+		}
 	}
 }
 
